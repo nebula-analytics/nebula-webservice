a025f5ff8d67f4da29e6ff2a5d3334c44e08ae50
Avrami Hammer <avrami.hammer@rmit.edu.au>
2019-11-03 14:15:02 +1100

    Create APIv2 which fixes timing issues found in v1 Apply temp fix to API v1

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ config["DOMAIN"]["joint"] = {
     'datasource': {
         'source': "views",
         'aggregation': {
+            'options': {
+                "allowDiskUse": True
+            },
             'pipeline': [
                 {"$match": {"at": {"$gte": datetime.now() - timedelta(minutes=30)}}},
                 # {"$match": "$where$"},
@@ -27,6 +30,7 @@ config["DOMAIN"]["joint"] = {
                     "foreignField": "doc_id",
                     "as": "matched"
                 }},
+
                 {"$match": {"matched": {"$not": {"$eq": []}}}},
                 {"$match": {"matched.status": {"$eq": "processed"}}},
                 {"$unwind": "$matched"},
@@ -53,10 +57,59 @@ app = Eve(settings=config)
 CORS(app)
 
 
+def clean_bad_dates(values: list):
+    new_value = []
+    for value in values:
+        if "$gte" in value["at"]:
+            key = "$gte"
+        else:
+            key = "$lte"
+        try:
+            """Validate date value and convert to mongo-friendly"""
+            # actual_value = parse(value["at"][key])
+            actual_value = datetime.fromisoformat(value["at"][key])
+            value["at"][key] = actual_value
+            new_value.append(value)
+        except (ValueError, TypeError):
+            pass
+    values.clear()
+    values.extend(new_value)
+
+
+def set_default_times(values: list):
+    default_window = timedelta(minutes=30)
+    if not values:
+        values.append({"at": {"$gte": (datetime.utcnow() - default_window)}})
+    if len(values) == 1:
+        value = values[0]["at"]
+        if "$gte" in value:
+            date_value = value["$gte"]
+            values.append(
+                {"at": {"$lte": (date_value + default_window)}}
+            )
+        else:
+            date_value = value["$lte"]
+            values.append(
+                {"at": {"gte": (date_value - default_window)}}
+            )
+
+
+def pre_views_get_callback(endpoint: str, pipeline: list):
+    if endpoint == "views":
+        """"""
+        times = pipeline[0].get("$match").get("$and")
+        clean_bad_dates(times)
+        set_default_times(times)
+    return
+
+
 @app.route("/application/config")
 def get_config():
     return jsonify(ConfigMap.theme)
 
+
+app.before_aggregation += pre_views_get_callback
+# app.after_aggregation += pre_views_get_callback
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0")
